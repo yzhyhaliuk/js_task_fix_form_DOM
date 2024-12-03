2b5e71691e2af8fd4811ead8ccdde54e754d184a
Yana <anazigaluk@gmail.com>
2024-12-03 22:41:51 +0200

    make build

---
+++ index.75355e4c.js
@@ -0,0 +1,2 @@
+document.querySelectorAll("form input").forEach(function(e){var t=document.createElement("label");t.className="field-label",t.setAttribute("for",e.id),t.textContent=e.name,e.parentElement.appendChild(t);var a=e.name.charAt(0).toUpperCase()+e.name.slice(1);e.setAttribute("placeholder",a)});
+//# sourceMappingURL=index.75355e4c.js.map
